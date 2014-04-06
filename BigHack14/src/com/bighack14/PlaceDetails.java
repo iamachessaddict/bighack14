2ab377ebcc094fa363ef76c8c6670f55f749337e
Zheng Zhu <zhengzhu88@gmail.com>
2014-04-06 12:16:44 -0700

    still nonfunctional but has some API stuff

---
+++ BigHack14/src/com/bighack14/PlaceDetails.java
@@ -0,0 +1,21 @@
+package com.bighack14;
+
+import java.io.Serializable;
+
+import com.google.api.client.util.Key;
+public class PlaceDetails implements Serializable {
+	 
+    @Key
+    public String status;
+     
+    @Key
+    public Place result;
+ 
+    @Override
+    public String toString() {
+        if (result!=null) {
+            return result.toString();
+        }
+        return super.toString();
+    }
+}
---
+++ BigHack14/src/com/bighack14/PlacesList.java
@@ -0,0 +1,7 @@
+package com.bighack14;
+
+import java.io.Serializable;
+
+public class PlacesList implements Serializable {
+
+}
